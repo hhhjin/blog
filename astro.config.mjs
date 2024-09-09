b7c261ff4e173d31c5ace4f1ffa83c1866e19c0a
hhhjin <kkp7125@gmail.com>
2024-09-09 14:28:49 +0900

    2024-09-09

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import prismjs from "vite-plugin-prismjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hyeongjin.com",
   integrations: [
     tailwind(),
-    react({
-      include: ["**/react/*"],
-    }),
+    // react({
+    //   include: ["**/react/*"],
+    // }),
     sitemap(),
   ],
-  vite: {
-    plugins: [
-      prismjs({
-        languages: ["bash", "tsx"],
-      }),
-    ],
-  },
 });
